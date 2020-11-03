51b2302128d013efb2325da0437db5440e9e5665
Matthieu-CM <matthieu.correia-moreira@epitech.eu>
2020-11-03 20:04:06 +0100

    [ADD] unknown command

---
+++ add.js
@@ -0,0 +1,19 @@
+const fs = require("fs")
+
+function Add(message, args, command) {
+    console.log("COUCOUA")
+    if (message.author.username === "Zuma Torney" || message.author.username === "Cerfio" || message.author.username === "Rajie") {
+        console.log("COUCOU")
+        if (args[0] !== undefined && args[1] !== undefined) {
+            let register = require("./registered.json")
+            register[args[0]] = args[1]
+            fs.writeFile("./registered.json", JSON.stringify(register), function (err) {
+                if (err) throw err;
+                console.log('Saved!');
+            });
+            message.channel.send("Command added")
+        }
+    }
+}
+
+exports.Add = Add;
