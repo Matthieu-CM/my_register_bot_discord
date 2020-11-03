e1dec8f11d032ec37c3fb6efae64368ab853eb55
Cerfio <yannis.coulibaly@epitech.eu>
2020-11-03 19:56:15 +0100

    [ADD] call to send a message to user

--- index.js
+++ index.js
@@ -11,55 +11,73 @@ const List = require("./list")
 const Registration = require("./registration")
 const Music = require("./music")
 
+function Call(message) {
+    let registered = require("./registered.json");
+    console.log(registered.today);
+    if (registered.today.length === 0) {
+        message.reply("Personne n'a dit être disponible, RT si t triste");
+    } else {
+        registered.today.forEach((element) => {
+            // console.log(element);
+            // channel.send("<@" + message.author.id + ">");
+            // message.channel.send('<@' + 'Cerfio' + '>');
+            // console.log('author', message.id);
+            // message.channel.send(`<@Cerfio> you just got a hug`);
+            // message.channel.send(`Hey ${message.author} how's it going?`);
+        })
+    }
+}
 
 client.on("message", function (message) {
     if (message.author.bot) return;
     if (message.author.username === "bod") {
-        message.reply("Toi je t'aime pas")
+        message.reply("Toi je t'aime pas");
     }
     if (!message.content.startsWith(prefix)) return;
     if (message.channel.id !== process.env.channel_id) return;
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
-    console.log("Command => ", message.author.username, command, args)
+    console.log("Command => ", message.author.username, command, args);
     if (command === "register") {
-        Registration.Register(message)
+        Registration.Register(message);
     } else if (command === "unregister") {
-        Registration.Unregister(message)
+        Registration.Unregister(message);
+    } else if (command === "call") {
+        Call(message);
     } else if (command === "cerfio") {
-        message.channel.send("Oui maitre")
+        message.channel.send("Oui maitre");
     } else if (command === "help") {
-        message.channel.send("```\r!register:\tPermet de s'enregistrer pour ce soir\r!unregister:\tPermet d'annuler sa participation\r!list:\tPermet d'afficher la liste des participants\r!clear:\rPermet de réinitialiser la liste des participants\r!help: Pas besoin de dire plus\r\rBot made by Zuma Torney <3\r```")
+        message.channel.send("```\r!register:\tPermet de s'enregistrer pour ce soir\r!unregister:\tPermet d'annuler sa participation\r!list:\tPermet d'afficher la liste des participants\r!clear:\rPermet de réinitialiser la liste des participants\r!help: Pas besoin de dire plus\r\rBot made by Zuma Torney <3\r```");
     } else if (command === "buzz") {
-        message.channel.send("nik ta mere")
+        message.channel.send("nik ta mere");
     } else if (command === "clear") {
-        let registered = require("./registered.json")
-        registered.today = []
-        Registration.updateRegistered(registered)
-        message.channel.send("Liste nettoyé")
+        let registered = require("./registered.json");
+        registered.today = [];
+        Registration.updateRegistered(registered);
+        message.channel.send("Liste nettoyé");
     } else if (command === "list") {
-        List.List(message)
+        List.List(message);
     } else if (command === "code") {
-        Code.Code(message, args)
+        Code.Code(message, args);
     } else if (command === "objection") {
-        Music.playMusic(client, message, "https://www.youtube.com/watch?v=fR4P8o95WPA")
+        Music.playMusic(client, message, "https://www.youtube.com/watch?v=fR4P8o95WPA");
     } else if (command === "snk") {
-        Music.playMusic(client, message, "https://youtu.be/pa00z_Bp2j4?t=161")     
+        Music.playMusic(client, message, "https://youtu.be/pa00z_Bp2j4?t=161")
     } else if (command === "leave") {
-        Music.leave(message)     
+        Music.leave(message);
     } else if (command === "add") {
         if (args[0] !== undefined && args[1] !== undefined) {
-            let register = require("./registered.json")
-            register[args[0]] = args[1]
+            let register = require("./registered.json");
+            register[args[0]] = args[1];
             fs.writeFile("./registered.json", JSON.stringify(register), function (err) {
                 if (err) throw err;
                 console.log('Saved!');
               });
-            message.channel.send("Command added")
+            message.channel.send("Command added");
         }
     } else {
-        let register = require("./registered.json")
+        let register = require("./registered.json");
         if (register[command] !== undefined) {
             Music.playMusic(client, message, register[command])
         }
