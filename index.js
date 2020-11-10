06f29501d0fe829de7671cb5199075d0e7d9b536
Matthieu-CM <matthieu.correia-moreira@epitech.eu>
2020-11-10 18:49:49 +0100

    [FIX] bug

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ client.on("message", function (message) {
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
-<<<<<<< Updated upstream
     console.log("Command => ", message.author.username, command, args)
     if (command === "shutdown" && (admin.indexOf(message.author.username) !== -1)) {
         shutdown = true;
@@ -53,8 +52,6 @@ client.on("message", function (message) {
         Code.Code(message, args)
         return
     }
-=======
->>>>>>> Stashed changes
     if (message.channel.id !== process.env.CHANNEL_ID) return;
     if (command === "register") {
         Registration.Register(message);
